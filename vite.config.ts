32c428799d7bfcd9d5df747233c5d5831e315582
Theeussx <teteutv69@gmail.com>
2026-06-07 17:50:47 -0300

    Update tanstackStart import and re-enable react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsConfigPaths from 'vite-tsconfig-paths';
-import { tanstackStart } from '@tanstack/start-vite-plugin';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
-    react(),
     tanstackStart({
       server: { entry: 'src/server.ts' },
     }),
+    react(),
   ],
   server: {
     port: 3000,
